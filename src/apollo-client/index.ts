070fec123c3211ff05f65682e12cf9891d8d3621
Raymond Johannessen <rajohan1@gmail.com>
2020-05-24 04:32:59 +0200

    Switched from Relay to Apollo Client. Authentication should now be working completely

--- src/apollo-client/index.ts
+++ src/apollo-client/index.ts
@@ -1,9 +1,24 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
 import { authLink, errorLink, httpLink } from "./links";
+
+const defaultOptions: DefaultOptions = {
+    watchQuery: {
+        fetchPolicy: "cache-and-network",
+        errorPolicy: "ignore"
+    },
+    query: {
+        fetchPolicy: "cache-first",
+        errorPolicy: "all"
+    },
+    mutate: {
+        errorPolicy: "all"
+    }
+};
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: errorLink.concat(authLink.concat(httpLink))
+    link: errorLink.concat(authLink.concat(httpLink)),
+    defaultOptions
 });
 
 export { client };
